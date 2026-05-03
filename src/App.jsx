01864e0a3500048e52eb068b8305e8829c700e35
Praful Karn <karnprafull321@gmail.com>
2026-05-03 15:56:25 +0545

    chore: import Login component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Login from './pages/Login'
 
 function App() {
   return (
