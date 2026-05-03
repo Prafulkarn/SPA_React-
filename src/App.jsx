3d46e1962dcd2f62f2290514c8b3717fe743e019
Praful Karn <karnprafull321@gmail.com>
2026-05-03 15:56:52 +0545

    feat: add Login route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   )
